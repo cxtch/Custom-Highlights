da98949af105949948c8d4f7a6cc9ab810e12fa4
cxtch <iain.sh.li@gmail.com>
2022-01-23 11:26:53 -0500

    colours

--- colours/colours.js
+++ colours/colours.js
@@ -1,14 +1,14 @@
 const ESC = '\x1b';
 const colours = new Map([
-  ['grey', '\x1b[1;30m'],
-  ['red', '\x1b[1;31m'],
-  ['green', '\x1b[1;32m'],
-  ['gold', '\x1b[1;33m'],
-  ['blue', '\x1b[1;34m'],
-  ['magenta', '\x1b[1;35m'],
-  ['aqua', '\x1b[1;36m'],
-  ['white', '\x1b[1;37m'],
-  ['neutral', '\x1b[1;0m']
+  ['grey', '\x1b[0;30m'],
+  ['red', '\x1b[0;31m'],
+  ['green', '\x1b[0;32m'],
+  ['gold', '\x1b[0;33m'],
+  ['blue', '\x1b[0;34m'],
+  ['magenta', '\x1b[0;35m'],
+  ['aqua', '\x1b[0;36m'],
+  ['white', '\x1b[0;37m'],
+  ['neutral', '\x1b[0;0m']
 ]),
   //Just for redundancy.
   coloursArray = ['grey', 'red', 'green', 'gold', 'blue', 'magenta', 'aqua', 'white', 'neutral'];
